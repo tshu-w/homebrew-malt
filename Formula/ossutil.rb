9200c3dc3694de4e28b045b1a9d1f60003a9bf06
Tianshu Wang <wang@tianshu.me>
2025-11-11 15:12:49 +0800

    Add stable v1.7.19 tarball and checksum

--- Formula/ossutil.rb
+++ Formula/ossutil.rb
@@ -2,13 +2,16 @@ class Ossutil < Formula
   desc "Command-line utility for Alibaba Cloud OSS"
   homepage "https://github.com/aliyun/ossutil"
 
+  url "https://github.com/aliyun/ossutil/archive/refs/tags/v1.7.19.tar.gz"
+  sha256 "25fb364cb1d1eb254a2629359c5e0c94a68e352846caeed0b45c971ac9a17dd5"
+  version "1.7.19"
+
   head "https://github.com/aliyun/ossutil.git", branch: "master"
 
   depends_on "go" => :build
 
   def install
-    ENV["GOBIN"] = bin
-    system "go", "install", "github.com/aliyun/ossutil@latest"
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "."
   end
 
   test do

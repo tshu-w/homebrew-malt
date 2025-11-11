f50773b81151bbf0088306f3194c4b4c3702b8e3
Tianshu Wang <wang@tianshu.me>
2025-11-11 17:04:11 +0800

    Upgrade ossutil to 2.2.0

--- Formula/ossutil.rb
+++ Formula/ossutil.rb
@@ -1,21 +1,42 @@
 class Ossutil < Formula
   desc "Command-line utility for Alibaba Cloud OSS"
-  homepage "https://github.com/aliyun/ossutil"
+  homepage "https://help.aliyun.com/zh/oss/developer-reference/ossutil-overview/"
 
-  url "https://github.com/aliyun/ossutil/archive/refs/tags/v1.7.19.tar.gz"
-  sha256 "25fb364cb1d1eb254a2629359c5e0c94a68e352846caeed0b45c971ac9a17dd5"
-  version "1.7.19"
+  version "2.2.0"
 
-  head "https://github.com/aliyun/ossutil.git", branch: "master"
+  livecheck do
+    url :homepage
+    regex(%r{ossutil/v2/(\d+(?:\.\d+)+)/ossutil-\1-(?:mac|linux)-(?:amd64|arm64)\.zip}i)
+  end
 
-  depends_on "go" => :build
+  on_macos do
+    on_intel do
+      url "https://gosspublic.alicdn.com/ossutil/v2/2.2.0/ossutil-2.2.0-mac-amd64.zip"
+      sha256 "6b5fd4902683817e6b419db9ee4b1cb825142e4b95ee603f8aa8e373a69e6bfa"
+    end
+    on_arm do
+      url "https://gosspublic.alicdn.com/ossutil/v2/2.2.0/ossutil-2.2.0-mac-arm64.zip"
+      sha256 "dc5b73cde2da84c0e2a13935e63bf419a029fac739cfd6febff9a3ad46af22c3"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://gosspublic.alicdn.com/ossutil/v2/2.2.0/ossutil-2.2.0-linux-amd64.zip"
+      sha256 "9e02837d806cfe976ae6c1fc22557d8e0a394ca6d298b45fb9f48a360d3a67f4"
+    end
+    on_arm do
+      url "https://gosspublic.alicdn.com/ossutil/v2/2.2.0/ossutil-2.2.0-linux-arm64.zip"
+      sha256 "4f76dfd71d2af8265fcb9309b530f4671242cf5993a8fd0f0e089de7e9665f72"
+    end
+  end
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w"), "."
+    # Archive contains a directory named like ossutil-<ver>-mac-<arch>/ossutil
+    bin.install Dir["**/ossutil"].first => "ossutil"
   end
 
   test do
-    # Expect help text or version output without crashing
     output = shell_output("#{bin}/ossutil --help")
     assert_match "ossutil", output
   end

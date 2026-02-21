5ad14d6b8a6d82ea145c131724c9d1c6b01c565c
Tianshu Wang <wang@tianshu.me>
2026-02-21 20:05:08 +0800

    Add autobump workflow for pi-mono

--- Formula/pi-mono.rb
+++ Formula/pi-mono.rb
@@ -1,24 +1,30 @@
 class PiMono < Formula
   desc "Minimal terminal coding harness"
   homepage "https://github.com/badlogic/pi-mono"
-  version "0.52.12"
+  version "0.54.0"
   license "MIT"
+
+  livecheck do
+    url :stable
+    regex(/v?(\d+(?:\.\d+)+)/i)
+    strategy :github_latest
+  end
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/badlogic/pi-mono/releases/download/v0.52.12/pi-darwin-arm64.tar.gz"
-      sha256 "c7c5d1987e4414cf7982b232ea20ade59905824df0db0bf696efc3aff5000d46"
+      url "https://github.com/badlogic/pi-mono/releases/download/v0.54.0/pi-darwin-arm64.tar.gz"
+      sha256 "9b17d65c32ea75059d5556ee7862e4df75e7f8fb1ee1623bf6da20dbfba8a9f9"
     else
-      url "https://github.com/badlogic/pi-mono/releases/download/v0.52.12/pi-darwin-x64.tar.gz"
-      sha256 "8a2127785a2c1b3bee36a79b2539342d48b46c1aa782f6ad66175c5d7af17d68"
+      url "https://github.com/badlogic/pi-mono/releases/download/v0.54.0/pi-darwin-x64.tar.gz"
+      sha256 "5675694c87c1e2923c877e991f87105f4096186b86c367626970783234d38ac2"
     end
   elsif OS.linux?
     if Hardware::CPU.arm?
-      url "https://github.com/badlogic/pi-mono/releases/download/v0.52.12/pi-linux-arm64.tar.gz"
-      sha256 "e489e9b08941a488b6ef76cf0b97118a546cbb46ed8347ca990429f8377880b0"
+      url "https://github.com/badlogic/pi-mono/releases/download/v0.54.0/pi-linux-arm64.tar.gz"
+      sha256 "e5abcd57371dad6cb0af1092d9697a661d03d4e55411acfb1048d3988f500076"
     else
-      url "https://github.com/badlogic/pi-mono/releases/download/v0.52.12/pi-linux-x64.tar.gz"
-      sha256 "314ffbb4255adc4511a3b2e2d994ffe0aaae92dedd04ff2d80faff034d6cc073"
+      url "https://github.com/badlogic/pi-mono/releases/download/v0.54.0/pi-linux-x64.tar.gz"
+      sha256 "5f17551ed731cef3cbac51526aa6e77dc2f86db3239dfcb32b5a4f70c75b26f7"
     end
   end
 

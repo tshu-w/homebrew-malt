df89834191a5a3d9e54c991bc901f395d302f92d
Tianshu Wang <wang@tianshu.me>
2026-06-28 14:43:43 +0800

    chore(casks): revise MonoLisa custom features

--- Casks/font-monolisa-custom.rb
+++ Casks/font-monolisa-custom.rb
@@ -1,10 +1,10 @@
 cask 'font-monolisa-custom' do
-  version "3.000"
-  sha256 "572279ea6dcd75bd08cdae9b5c156b6290e7907c4204c5d2b678ec71b2f3021a"
+  version "3.000,1"
+  sha256 "59976147f0000978bd057a45c18ff4ccf0c93a7480b28d9b13b611ed59feef2b"
 
-  name "MonoLisa Code/Text Custom (zero, ss03, ss07)"
+  name "MonoLisa Code/Text Custom (zero, ss03, ss11)"
   homepage "https://monolisa.dev"
-  url "https://www.dropbox.com/scl/fi/0fttw8hzzqc9ubrqldnad/MonoLisa-#{version}-zero_ss03_ss07.zip?rlkey=1nhooaao52j4o9qrllilbh8or&dl=1",
+  url "https://www.dropbox.com/scl/fi/0fttw8hzzqc9ubrqldnad/MonoLisa-#{version.before_comma}-zero_ss03_ss07.zip?rlkey=1nhooaao52j4o9qrllilbh8or&dl=1",
       verified: "dropbox.com/"
 
   font "ttf/MonoLisaCodeCustomItalic.ttf"

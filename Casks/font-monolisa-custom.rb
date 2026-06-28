8ba2c703e1d4bce689edc54c032bca0e9a06ac2c
Tianshu Wang <wang@tianshu.me>
2026-06-28 22:35:38 +0800

    chore(casks): rename MonoLisa font families

--- Casks/font-monolisa-custom.rb
+++ Casks/font-monolisa-custom.rb
@@ -1,14 +1,14 @@
 cask 'font-monolisa-custom' do
-  version "3.000,1"
-  sha256 "59976147f0000978bd057a45c18ff4ccf0c93a7480b28d9b13b611ed59feef2b"
+  version "3.000,2"
+  sha256 "82002a2fd78b22e01d1948793fda9ce10a81c2c16a1dc06e38289fe606569bfd"
 
-  name "MonoLisa Code/Text Custom (zero, ss03, ss11)"
+  name "MonoLisa Code/Text (zero, ss03, ss11)"
   homepage "https://monolisa.dev"
-  url "https://www.dropbox.com/scl/fi/0fttw8hzzqc9ubrqldnad/MonoLisa-#{version.before_comma}-zero_ss03_ss07.zip?rlkey=1nhooaao52j4o9qrllilbh8or&dl=1",
+  url "https://www.dropbox.com/scl/fi/0fttw8hzzqc9ubrqldnad/MonoLisa-#{version.before_comma}-zero_ss03_ss11.zip?rlkey=1nhooaao52j4o9qrllilbh8or&dl=1",
       verified: "dropbox.com/"
 
-  font "ttf/MonoLisaCodeCustomItalic.ttf"
-  font "ttf/MonoLisaCodeCustomUpright.ttf"
-  font "ttf/MonoLisaTextCustomItalic.ttf"
-  font "ttf/MonoLisaTextCustomUpright.ttf"
+  font "ttf/MonoLisaCodeItalic.ttf"
+  font "ttf/MonoLisaCodeUpright.ttf"
+  font "ttf/MonoLisaTextItalic.ttf"
+  font "ttf/MonoLisaTextUpright.ttf"
 end
